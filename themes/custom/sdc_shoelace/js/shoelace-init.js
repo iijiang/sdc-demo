6c6e5c50a93ef18e5f4a9b47daa428465076ea5f
Yi Jiang <yi.jiang@education.gov.au>
2025-10-27 20:36:23 +1100

    Populate componets

---
+++ themes/custom/sdc_shoelace/js/shoelace-init.js
@@ -0,0 +1,8 @@
+/**
+ * Initialize Shoelace Web Components
+ */
+import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.1/cdn/shoelace-autoloader.js';
+
+// Set the base path for Shoelace assets
+import { setBasePath } from 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.1/cdn/utilities/base-path.js';
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.1/cdn/');
